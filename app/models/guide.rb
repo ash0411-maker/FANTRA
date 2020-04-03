48c0d35cd373c96826d03228ffddc8f02bf61a59
ash <uetani.0411@gmail.com>
2020-04-04 00:49:25 +0900

    [add]バリデーション

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -7,4 +7,12 @@ class Guide < ApplicationRecord
   attachment :identification_image
 
   has_many :tours
+
+  validates :name, presence: true
+  validates :family_name, presence: true
+  validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :identification_image, presence: true
+  validates :phone_number, presence: true
+  validates :introduction, presence: true
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -4,4 +4,13 @@ class Tour < ApplicationRecord
 	belongs_to :city
 	has_many :tour_photos, dependent: :destroy
   	accepts_attachments_for :tour_photos, attachment: :image
+
+  	validates :genre_id, presence: true
+  	validates :city_id, presence: true
+  	validates :title, presence: true
+  	validates :body, presence: true
+  	validates :capacity, presence: true
+  	validates :price, presence: true
+  	validates :content_of_price, presence: true
+
 end
