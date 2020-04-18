c9a39e5bb8fddf7d4f30d9b98798b3a99e907844
satoshi uetani <satoshi.0411@icloud.com>
2020-04-18 21:22:59 +0000

    [modified]Tourist チャット通知機能　kaminari

--- app/controllers/tourist/chat_notices_controller.rb
+++ app/controllers/tourist/chat_notices_controller.rb
@@ -1,7 +1,25 @@
 class Tourist::ChatNoticesController < ApplicationController
 
   def index
-  	@chat_notices = current_tourist.tourist_passive_notices.page(params[:page]).per(20)
+  	@chat_notices = current_tourist.tourist_passive_notices.joins(:message).where('messages.is_tourist = ?',0).page(params[:page]).per(20)
   end
 
 end
+
+
+
+# @chat_notices = []
+# chat_notices.each do |notice|
+# 	if notice.message.is_tourist == false
+# 		@chat_notices << notice
+# 	end
+# end
+# @chat_notices.page(params[:page]).per(20)
+
+
+
+
+
+# @chat_notices = current_tourist.tourist_passive_notices.page(params[:page]).per(20)
+# 上記を用いてview上で条件分岐で「if message.is_tourist == false」としていたが、
+# これだとページングで問題が出るため修正した。
