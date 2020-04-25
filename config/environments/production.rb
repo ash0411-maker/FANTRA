1546f4d7cb58fdcd082cdc9661ee6a138f1f2134
ash <uetani.0411@gmail.com>
2020-04-25 17:46:34 +0900

    [modified]admin 注文詳細ページ　ID番号追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
+# ---- ActionMailer ----------
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
