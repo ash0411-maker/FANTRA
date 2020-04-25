1a22712fd0f81401881574a374a9c0df070f5346
ash <uetani.0411@gmail.com>
2020-04-25 17:42:03 +0900

    [add]ActionMailer server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,6 +72,9 @@ Rails.application.configure do
   #   enable_starttls_auto: true
   # }
 
+
+
+# ---- ActionMailer ----------
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
