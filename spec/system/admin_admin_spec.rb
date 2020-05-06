382e4ae2e1fc42570412613984bdc46e1510f446
satoshi uetani <satoshi.0411@icloud.com>
2020-05-06 06:12:02 +0000

    [add]admin cityページテスト

--- spec/system/admin_admin_spec.rb
+++ spec/system/admin_admin_spec.rb
@@ -29,6 +29,51 @@ RSpec.describe Admins::SessionsController, type: :system do
       end
     end
   end
+
+  describe '編集のテスト' do
+    context '自分の編集画面への遷移' do
+      it '遷移できる' do
+        visit edit_user_path(user)
+        expect(current_path).to eq('/users/' + user.id.to_s + '/edit')
+      end
+    end
+    context '他人の編集画面への遷移' do
+      it '遷移できない' do
+        visit edit_user_path(test_user2)
+        expect(current_path).to eq('/users/' + user.id.to_s)
+      end
+    end
+
+    # context '表示の確認' do
+    #   before do
+    #     visit edit_user_path(user)
+    #   end
+    #   it 'User infoと表示される' do
+    #     expect(page).to have_content('User info')
+    #   end
+    #   it '名前編集フォームに自分の名前が表示される' do
+    #     expect(page).to have_field 'user[name]', with: user.name
+    #   end
+    #   it '画像編集フォームが表示される' do
+    #     expect(page).to have_field 'user[profile_image]'
+    #   end
+    #   it '自己紹介編集フォームに自分の自己紹介が表示される' do
+    #     expect(page).to have_field 'user[introduction]', with: user.introduction
+    #   end
+    #   it '編集に成功する' do
+    #     click_button 'Update User'
+    #     expect(page).to have_content 'successfully'
+    #     expect(current_path).to eq('/users/' + user.id.to_s)
+    #   end
+    #   it '編集に失敗する' do
+    #     fill_in 'user[name]', with: ''
+    #     click_button 'Update User'
+    #     expect(page).to have_content 'error'
+    #     #もう少し詳細にエラー文出したい
+    #     expect(current_path).to eq('/users/' + user.id.to_s)
+    #   end
+    # end
+  end
 end
 
 
--- spec/system/admin_city_spec.rb
+++ spec/system/admin_city_spec.rb
@@ -40,4 +40,26 @@ RSpec.describe 'Cityに関するテスト', type: :system do
       end
     end
   end
+
+
+
+  describe 'Cityページのテスト' do
+    context '自分の編集画面への遷移' do
+      before do
+        login admin
+        visit admin_cities_path
+      end
+      it '遷移できる' do
+        expect(current_path).to eq('/admin/cities')
+      end
+    end
+    context '他人の編集画面への遷移' do
+      before do
+        visit admin_cities_path
+      end
+      it '遷移できない' do
+        expect(current_path).to eq('/admins/sign_in')
+      end
+    end
+  end
 end
