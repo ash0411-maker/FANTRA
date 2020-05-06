b5491921eeffa519d9068efed40614c8c109a7b9
satoshi uetani <satoshi.0411@icloud.com>
2020-05-06 06:51:01 +0000

    [add]Guide チャットルームページテスト

--- spec/system/tour_guide_guide_spec.rb
+++ spec/system/tour_guide_guide_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe TourGuide::GuidesController, type: :system do
     before do
       visit new_guide_registration_path
     end
+
+
     context '新規登録画面' do
       it '新規登録に成功する' do
         fill_in 'guide[name]', with: Faker::Name.first_name
@@ -50,10 +52,11 @@ RSpec.describe TourGuide::GuidesController, type: :system do
 
 
   describe 'Guideログイン' do
-    let(:guide) { create(:guide) }
     before do
       visit new_guide_session_path
     end
+
+
     context 'ログイン画面に遷移' do
       let(:test_guide) { guide }
       it 'ログインに成功する' do
@@ -73,6 +76,86 @@ RSpec.describe TourGuide::GuidesController, type: :system do
       end
     end
   end
+
+
+  describe 'Guide編集ページのテスト' do
+    let(:test_guide2) { create(:guide) }
+    before do
+      login guide
+    end
+
+
+    context '自分の編集画面への遷移' do
+      it '遷移できる' do
+        visit edit_tour_guide_guide_path(guide)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s + '/edit')
+      end
+    end
+    context '他人の編集画面への遷移' do
+      it '遷移できない' do
+        visit edit_tour_guide_guide_path(test_guide2)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s)
+      end
+    end
+  end
+
+
+  describe 'Guide予約ページのテスト' do
+    let(:test_guide2) { create(:guide) }
+    before do
+      login guide
+    end
+
+
+    context '自分の予約画面一覧への遷移' do
+      it '遷移できる' do
+        visit tour_guide_guide_orders_path(guide)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s + '/orders')
+      end
+    end
+    context '他人の予約画面への遷移' do
+      it '遷移できない' do
+        visit tour_guide_guide_orders_path(test_guide2)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s)
+      end
+    end
+  end
+
+
+  describe 'Guideチャットルームページのテスト' do
+    let(:test_guide2) { create(:guide) }
+    let(:room) { create(:room) }
+    before do
+      login guide
+    end
+
+
+    context '自分のチャットルーム一覧への遷移' do
+      it '遷移できる' do
+        visit tour_guide_guide_rooms_path(guide)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s + '/rooms')
+      end
+    end
+    context '他人のチャットルーム一覧への遷移' do
+      it '遷移できない' do
+        visit tour_guide_guide_rooms_path(test_guide2)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s)
+      end
+    end
+    # context '自分のチャットルームへの遷移' do
+    #   it '遷移できる' do
+    #     visit tour_guide_guide_room_path(guide, room)
+    #     expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s + '/rooms/' + room.id.to_s)
+    #   end
+    # end
+    context '他人のチャットルームへの遷移' do
+      it '遷移できない' do
+        visit tour_guide_guide_room_path(test_guide2, room)
+        expect(current_path).to eq('/tour_guide/guides/' + guide.id.to_s)
+      end
+    end
+  end
+
 end
 
 
